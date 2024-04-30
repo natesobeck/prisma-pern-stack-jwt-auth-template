69f4b8cd5128c2106c05a8483b0ae1c7755a923b
Nate Sobeck <nsobeck910@gmail.com>
2024-04-30 13:00:17 -0400

    add bin file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // npm packages
-import 'dotenv/config.js'
-import express from 'express'
-import logger from 'morgan'
-import cors from 'cors'
-import formData from 'express-form-data'
+import "dotenv/config.js"
+import express from "express"
+import logger from "morgan"
+import cors from "cors"
+import formData from "express-form-data"
 
 // import routes
 
@@ -12,7 +12,7 @@ const app = express()
 
 // basic middleware
 app.use(cors())
-app.use(logger('dev'))
+app.use(logger("dev"))
 app.use(express.json())
 app.use(formData.parse())
 
@@ -20,10 +20,10 @@ app.use(formData.parse())
 
 // handle 404 errors
 app.use(function (req, res, next) {
-    res.status(404).json({ err: 'Not found' })
+  res.status(404).json({ err: "Not found" })
 })
 
 // handle all other errors
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500).json({ err: err.message })
+  res.status(err.status || 500).json({ err: err.message })
 })
